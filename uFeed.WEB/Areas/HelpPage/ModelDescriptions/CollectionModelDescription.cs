98f7c09e69debf83c777d8aace426b119dc55f4e
iordanov.serhii <sergey.ior97@gmail.com>
2016-11-12 11:41:38 +0200

    Add project files.

---
+++ uFeed.WEB/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace uFeed.WEB.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
