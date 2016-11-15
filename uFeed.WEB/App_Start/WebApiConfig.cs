340f941348403051a09c80c70a1451cb28c9b42c
Oleksii Budianskyi <oleksii_budianskyi.com>
2016-11-15 17:05:09 +0200

    Add cross-origin

--- uFeed.WEB/App_Start/WebApiConfig.cs
+++ uFeed.WEB/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web.Http;
+using System.Web.Http.Cors;
 using Microsoft.Owin.Security.OAuth;
 
 namespace uFeed.WEB
@@ -7,6 +8,9 @@ namespace uFeed.WEB
     {
         public static void Register(HttpConfiguration config)
         {
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
+
             // Web API configuration and services
             // Configure Web API to use only bearer token authentication.
             config.SuppressDefaultHostAuthentication();
