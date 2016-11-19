0873363d1455d7bb377ffca221cbff2d38671175
iordanov.serhii <sergey.ior97@gmail.com>
2016-11-19 11:49:58 +0200

    Implement FacebookException as a realization of ISocialException

---
+++ uFeed.DAL.SocialService/Services/Facebook/FacebookException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using uFeed.DAL.SocialService.Interfaces;
+
+namespace uFeed.DAL.SocialService.Services.Facebook
+{
+    public class FacebookException : Exception, ISocialException
+    {
+        public bool IsNoAccessToken { get; set; }
+
+        public FacebookException(string message, bool isNoAccessToken = false) : base(message)
+        {
+            IsNoAccessToken = isNoAccessToken;
+        }
+    }
+}
