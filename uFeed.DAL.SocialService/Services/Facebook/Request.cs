739bef5403697790c9068431e620fc999481417c
iordanov.serhii <sergey.ior97@gmail.com>
2016-11-19 15:25:26 +0200

    implement new SocialApi interface

--- uFeed.DAL.SocialService/Services/Facebook/Request.cs
+++ uFeed.DAL.SocialService/Services/Facebook/Request.cs
@@ -1,5 +1,6 @@
 ﻿using System.IO;
 using System.Net;
+using uFeed.DAL.SocialService.Exceptions;
 
 namespace uFeed.DAL.SocialService.Services.Facebook
 {
@@ -7,30 +8,42 @@ namespace uFeed.DAL.SocialService.Services.Facebook
     {
         public static string ExecuteFacebookRequest(string request, string accessToken, string method)
         {
-            string result = "";
+            string result = string.Empty;
+
             if (method.Equals("get"))
             {
                 result = SendGetRequest("https://graph.facebook.com/v2.7/" + request + "&access_token=" + accessToken, null);
             }
+
             return result;
         }
         public static string SendGetRequest(string url, string data)
         {
             WebRequest req;
-            if (data == null || data.Equals(""))
-                req = WebRequest.Create(url);
-            else
-                req = WebRequest.Create(url + "?" + data);
-            WebResponse resp = req.GetResponse();
-            Stream stream = resp.GetResponseStream();
-            if (stream != null)
+
+            if (data == null || data.Equals(string.Empty))
             {
-                StreamReader sr = new StreamReader(stream);
-                var Out = sr.ReadToEnd();
-                sr.Close();
-                return Out;
+                req = WebRequest.Create(url);
             }
-            throw new FacebookException("Cannot get response stream");
+            else
+            {
+                req = WebRequest.Create(url + "?" + data);
+            }
+
+            WebResponse resp = req.GetResponse();
+
+            Stream stream = resp.GetResponseStream();
+
+            if (stream == null)
+            {
+                throw new SocialException("Cannot get response stream");
+            }
+
+            var sr = new StreamReader(stream);
+            var Out = sr.ReadToEnd();
+            sr.Close();
+
+            return Out;
         }
     }
 }
